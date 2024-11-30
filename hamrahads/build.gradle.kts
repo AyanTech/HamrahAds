3fffb531227bc5090ea71a4309e453d447b67f42
Moslem Khanbeiki <s.khanbeiki@gmail.com>
2024-11-30 16:07:28 +0330

    sync to jitpack

--- hamrahads/build.gradle.kts
+++ hamrahads/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.kotlin.kapt.plugin)
     alias(libs.plugins.kotlin.serialization.plugin)
+    id("maven-publish")
 }
 
 android {
@@ -27,11 +28,23 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
+    }
+}
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.ayantech"
+                artifactId = "AyanAds"
+                version = "0.1.5"
+            }
+        }
     }
 }
 
