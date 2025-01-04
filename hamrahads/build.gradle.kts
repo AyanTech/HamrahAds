fe17a31e0be05b41c0a6adcd1307bc7bb5f4a6f3
Moslem Khanbeiki <s.khanbeiki@gmail.com>
2025-01-04 11:44:19 +0330

    sync to jitpack 0.1.8

--- hamrahads/build.gradle.kts
+++ hamrahads/build.gradle.kts
@@ -42,7 +42,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.ayantech"
                 artifactId = "AyanAds"
-                version = "0.1.7"
+                version = "0.1.8"
             }
         }
     }
