35a987ae2944987d54e4aa6e642743785a52ead9
Moslem Khanbeiki <s.khanbeiki@gmail.com>
2025-09-17 12:03:08 +0330

    change trackers status

--- hamrahads/build.gradle.kts
+++ hamrahads/build.gradle.kts
@@ -42,7 +42,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.ayantech"
                 artifactId = "HamrahAds"
-                version = "0.1.36"
+                version = "0.1.37"
             }
         }
     }
